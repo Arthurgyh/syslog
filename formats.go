85b1b2201f9801cd70fe851bbb45ff707384ea13
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2015-10-12 22:14:34 +0200

    Update Nginx log_format
    Instead of using a predefined format explain the format and give an
    example.

--- formats.go
+++ formats.go
@@ -16,21 +16,22 @@ var (
 	// Nginx is the format to parse Nginx syslog messages. To allow the
 	// Message.Data to be filled the following logformat is required to be used:
 	//
-	//	log_format syslog '[request body_bytes_sent="$body_bytes_sent" '
-	//		'connection="$connection" '
-	//		'connection_requests="$connection_requests" '
-	//		'http_referer="$http_referer" '
-	//		'http_user_agent="$http_user_agent" '
-	//		'http_x_forwarded_for="$http_x_forwarded_for" '
-	//		'msec="$msec" '
+	//	log_format syslog '[request '
+	//		'variable="$variable" '
+	//		'variable2="$variable2" '
+	// 		']';
+	//
+	// For example:
+	//
+	//	log_format syslog '[request '
 	//		'remote_addr="$remote_addr" '
-	//		'remote_user="$remote_user" '
-	//		'request_length="$request_length" '
 	//		'request_time="$request_time" '
-	//		'status="$status"]';
+	//		'status="$status"'
+	// 		']';
 	//
 	// Using this log_format allows the Message.Data["Request"] to be filled with
-	// the data from Nginx.
+	// the data from Nginx. Using the above we can acces the status using
+	// Message.Data["Request"]["status"].
 	Nginx = nginxFormat
 )
 
