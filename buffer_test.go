0a384f12ab7ecd1c4c9e0068bf45f0dcf4f39606
Thomas de Zeeuw <thomasdezeeuw@gmail.com>
2015-10-16 18:43:12 +0200

    Add buffer tests
    This should raise the coverage to 100% for `buffer.go` file.

--- buffer_test.go
+++ buffer_test.go
@@ -13,7 +13,6 @@ func TestBuffer(t *testing.T) {
 	t.Parallel()
 
 	var msg = []byte("This is the very, very usefull message")
-
 	buf := newBuffer(msg)
 
 	if b, err := buf.Peek(4); err != nil {
@@ -40,13 +39,91 @@ func TestBuffer(t *testing.T) {
 		t.Fatalf("Expected buf.ReadSlice(' ') to return %s, but got %s", expected, got)
 	}
 
-	if b, err := buf.Peek(100); err != io.EOF {
-		t.Fatalf("Expected buf.Peek(100) to return error %s, but got %s", err.Error(), err)
-	} else if expected, got := "the very, very usefull message", string(b); got != expected {
-		t.Fatalf("Expected buf.Peek(100) to return %s, but got %s", expected, got)
-	}
-
 	if expected, got := "the very, very usefull message", string(buf.ReadAll()); got != expected {
 		t.Fatalf("Expected buf.ReadAll() to return %s, but got %s", expected, got)
+	}
+}
+
+func TestBufferDiscardTooMuch(t *testing.T) {
+	t.Parallel()
+
+	var msg = []byte("Some message")
+	buf := newBuffer(msg)
+
+	expected := len(msg)
+	discardLength := len(msg) + 1
+	n := buf.Discard(discardLength)
+
+	if n != expected {
+		t.Fatal("Expected buf.Discard(%d) to return %d, but got %d",
+			discardLength, expected, n)
+	}
+}
+
+func TestBufferPeekToomuch(t *testing.T) {
+	t.Parallel()
+
+	var msg = []byte("Some message")
+	buf := newBuffer(msg)
+
+	expected := "Some message"
+	peekLength := len(msg) + 1
+	b, err := buf.Peek(peekLength)
+	if err != io.EOF {
+		t.Fatalf("Expected buf.Peek(%d) to return error %s, but got %s",
+			peekLength, io.EOF.Error(), err.Error())
+	} else if got := string(b); got != expected {
+		t.Fatalf("Expected buf.Peek(%d) to return %s, but got %s", peekLength, expected, got)
+	}
+}
+
+func TestBufferReadLastByte(t *testing.T) {
+	t.Parallel()
+
+	buf := newBuffer([]byte{})
+
+	c, err := buf.ReadByte()
+	if err != io.EOF {
+		t.Fatalf("Expected buf.ReadByte() to return error %s, but got %s", io.EOF, err.Error())
+	} else if c != 0 {
+		t.Fatalf("Expected buf.ReadByte() to return 0, but got %q", string(c))
+	}
+}
+
+func TestBufferFirstUnreadByte(t *testing.T) {
+	t.Parallel()
+
+	buf := newBuffer([]byte{})
+
+	defer func() {
+		recv := recover()
+		if recv == nil {
+			t.Fatal("Expected a panic, but didn't get any")
+		}
+
+		got := recv.(string)
+		expected := "syslog: can't unread byte"
+		if got != expected {
+			t.Fatal("Expected panic value to be %q, but got %q", expected, got)
+		}
+	}()
+
+	buf.UnreadByte()
+}
+
+func TestBufferReadSliceNotFound(t *testing.T) {
+	t.Parallel()
+
+	var msg = []byte("Some-very-long-message-without-spaces")
+	buf := newBuffer(msg)
+
+	expected := "Some-very-long-message-without-spaces"
+	b, err := buf.ReadSlice(' ')
+	if err != io.EOF {
+		t.Fatalf("Expected buf.ReadSlice(' ') to return error %s, but got %s",
+			io.EOF.Error(), err.Error())
+	} else if got := string(b); got != expected {
+		t.Fatalf("Expected buf.ReadSlice(' ') to return %s, but got %s",
+			expected, got)
 	}
 }
